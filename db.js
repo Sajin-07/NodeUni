e4a580b7da0def0681806a5fd084ee789b3c2847
Sajin-07 <sifatsajin88@gmail.com>
2024-12-24 21:55:47 +0600

    Third commit

--- db.js
+++ db.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 
 // Define the MongoDB connection URL
 // const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
 // const mongoURL = process.env.MONGODB_URL;
-const mongoURL = 'mongodb://127.0.0.1:27017/university';
+// const mongoURL = 'mongodb://127.0.0.1:27017/university';
+// const mongoURL = 'mongodb+srv://sifatsajin88:HXPvIvsWwZU1mwfW@university.efxne.mongodb.net/university?retryWrites=true&w=majority';
+// const mongoURL = 'mongodb+srv://sifatsajin88:HXPvIvsWwZU1mwfW@university.efxne.mongodb.net/';
+
+const mongoURL = process.env.MONGODB_URL_ATLAS
+// const mongoURL = process.env.MONGODB_URL_LOCAL
 
 // Set up MongoDB connection
-// mongoose.connect(mongoURL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-mongoose.connect(mongoURL)
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+// mongoose.connect(mongoURL)
 
 // Get the default connection
 // Mongoose maintains a default connection object representing the MongoDB connection.
@@ -30,6 +35,7 @@ db.on('error', (err) => {
 db.on('disconnected', () => {
     console.log('MongoDB disconnected');
 });
+
 
 // Export the database connection
 module.exports = db;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.json()); // req.body
@@ -90,7 +91,7 @@ const studentRoutes = require('./routes/studentRoutes');
 
 app.use('/',studentRoutes);
 
-
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000');
 });
